11790c6baf3b09dce876093d8f2985073cf6798c
mioriaty <mioriaty@gmail.com>
2025-03-31 15:45:43 +0700

    update: styles

--- src/libs/components/post/post.tsx
+++ src/libs/components/post/post.tsx
@@ -12,7 +12,7 @@ export function Post(props: PostProps) {
   const { title, content, bannerImage, bannerImageHeight } = props;
 
   return (
-    <article className="w-full mb-10 flex flex-col items-center pt-20">
+    <article className="w-full mb-10 flex flex-col items-center pt-10 pl-3 pr-3">
       <h1 className="text-6xl font-black text-black mb-8">{title}</h1>
       <Image
         alt="Blog Image"
@@ -20,7 +20,7 @@ export function Post(props: PostProps) {
         // width={bannerImageWidth}
         width={'800'}
         height={bannerImageHeight}
-        className="[width: 800px]!"
+        className="[width: 800px]! max-w-full"
       />
       <div
         className="text-xl mt-4 max-w-3xl prose prose-p:text-black prose-headings:text-black"
