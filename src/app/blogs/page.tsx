70eb5256e59288dc03211e196d1dba2fe08a29ae
mioriaty <mioriaty@gmail.com>
2025-04-01 14:45:14 +0700

    update: update UI

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -5,7 +5,11 @@ import { BlogsList } from '@/features/blogs/blogs';
 export default function PostsPage() {
   return (
     <div className="container mx-auto px-4 py-12">
-      <h1 className="text-4xl font-bold  mb-8">Blog Posts</h1>
+      <div className="mb-8 space-y-2">
+        <h1 className="text-4xl font-bold">Blog Posts</h1>
+        <p className="text-muted-foreground">You kept me like a secret. But I kept you like an oath</p>
+      </div>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <BlogsList />
       </div>
